5718f972fe3aa7189eb6ad2e13f2cece8b2e31b3
jaygroover1 <jaygroover@gmail.com>
2019-07-08 21:56:09 -0400

    cards and socal

---
+++ app.js
@@ -0,0 +1,22 @@
+
+ function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+
+
+if(wScroll > $('#video').offet().top){
+    // console.log("")
+    $('#video').each(function(){
+        $('#video').addClass('.is-showing');
+    });
+}
+
+});
